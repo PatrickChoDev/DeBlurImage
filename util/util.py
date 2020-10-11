43bfb7a58b4c3ea63269096ca07058c3650fafab
Thanapat Chotipun <devpatrick.cho@gmail.com>
2020-10-11 22:36:40 +0700

    [Update] Create Images Function

--- util/util.py
+++ util/util.py
@@ -2,24 +2,35 @@ import os
 import numpy as np
 import PIL.Image as Image
 
-def isImageFile(file):
+def isImageFile(file : str) -> bool:
     EXTENSION = ['.jpg','.png','.jpeg']
-    if EXTENSION in file :
+    for EXTENSION in file :
         return True
     return False
 
-def list_image(directory,n_images):
+def listImages(directory) -> list:
     files = sorted(os.listdir(directory))
     return [os.path.join(directory, f) for f in files if isImageFile(f)]
 
-def loadImages(path):
-    return Image.open(path)
-
 def preprocessImages(image):
-    image = image.resize(256,256)
+    image = image.resize(720,720)
     img = np.array(image)
-    
-    return img
+    return img 
+
+def loadImages(path : str,folders : list,nImage=-1) -> dict:
+    if nImage < 0 :
+        nImage = float('inf')
+    blurPath,sharpPath = os.path.join(path,folders[0]),os.path.join(path,folders[1])
+    blurImagePath,sharpImagePath = listImages(blurPath),listImages(sharpPath)
+    blurImage,sharpImage = [],[]
+    for blur,sharp in zip(blurImagePath,sharpImagePath) :
+        blurImage.append(preprocessImages(Image.open(blur)))
+        sharpImage.append(preprocessImages(Image.open(sharp)))
 
 def deprocessImages():
     pass
+
+
+if __name__ == '__main__':
+    a = listImages('./datasets/sharp')
+    print(len(a))
