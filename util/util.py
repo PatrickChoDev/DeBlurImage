91ea3b747ed6b03d4d69634212ef50bb22a6e296
Thanapat Chotipun <devpatrick.cho@gmail.com>
2020-10-19 00:32:14 +0700

    [Update] 'preprocessImage' Finished

--- util/util.py
+++ util/util.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
 import PIL.Image as Image
+from numpy.lib.function_base import append
 
-def isImageFile(fileName : str) -> bool:
-    for extension in ['.jpeg','.png','.jpg']:
+def isImageFile(fileName: str) -> bool:
+    for extension in ['.jpeg', '.png', '.jpg']:
         if extension in fileName:
             return True
     return False
@@ -12,13 +13,19 @@ def listImages(directory) -> list:
     files = sorted(os.listdir(directory))
     return [os.path.join(directory, f) for f in files if isImageFile(f)]
 
-def preprocessImages(image : Image.Image) -> np.ndarray :
-    img = np.array(image)/255
-    imagePartForward = np.array([],dtype=float)
-    for xPart in range(img.shape[0]//256-1):
-        for yPart in range(img.shape[1]//256-1):
-            pass
-    return imagePartForward
+def preprocessImages(image):
+    img = np.array(image)
+    imagePartForward = []
+    imagePartBackward = []
+    for xPart in range(img.shape[0]//256):
+        for yPart in range(img.shape[1]//256):
+            forwardPart = img[xPart*256:xPart*256+256]
+            forwardPart = forwardPart.transpose(1,0,2)[yPart*256:yPart*256+256].transpose(1,0,2)
+            backwardPart = img[(img.shape[1]-xPart)*256-256:(img.shape[1]-xPart)*256]
+            backwardPart = backwardPart.transpose(1,0,2)[yPart*256:yPart*256+256].transpose(1,0,2)
+            imagePartForward.append(forwardPart)
+            imagePartBackward.append(backwardPart)
+    return {'Forward':imagePartForward,'Backward':imagePartBackward}
 
 def loadImages(path : str,folders : list,nImage=-1) -> dict:
     if nImage < 0 :
@@ -33,14 +40,15 @@ def loadImages(path : str,folders : list,nImage=-1) -> dict:
     return {
         'blur' : blurImage,
         'sharp': sharpImage,
-        'blurPath' : blurImagePath,
-        'sharpPath': sharpImagePath
+        'blurPath' : blurImagePath[:nImage],
+        'sharpPath': sharpImagePath[:nImage]
     }
 
 def deprocessImages():
     pass
 
 
+##For test Only
 if __name__ == '__main__':
     a = loadImages('./datasets/',['blurred','sharp'],1)
-    print(a['blur'])
+    print(a['blur'][0]['Forward'])
